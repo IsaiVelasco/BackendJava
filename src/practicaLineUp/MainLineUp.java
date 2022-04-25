eee8701ed00020171730f35ba3d0aaf99c02e486
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-04-25 00:07:11 -0500

    Prueba con exito conciertofull

--- src/practicaLineUp/MainLineUp.java
+++ src/practicaLineUp/MainLineUp.java
@@ -1,7 +1,6 @@
 package practicaLineUp;
 
 import javax.sound.sampled.*;
-import java.applet.AudioClip;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -15,15 +14,20 @@ public class MainLineUp {
 
         archivowav = new File(
                 FileSystems.getDefault().getPath("")
-                        .toAbsolutePath() + "/audio/concierto1.wav");
+                        .toAbsolutePath() + "/audio/conciertofull.wav");
 
         try {
             audioInputStream = AudioSystem.getAudioInputStream(archivowav);
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
+            System.out.println("audio abierto");
             clip.start();
+            Thread.sleep(32000);
+            System.out.println("audio iniciado");
         } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
             System.err.println(e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
     public static void play(){
