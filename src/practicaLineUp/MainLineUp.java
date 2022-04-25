d635db2b7c1713963c84e6620118111d6d8be02d
Isaí Velasco Hernández <velascohernandes.2017@gmail.com>
2022-04-24 23:56:37 -0500

    Prueba de audio v1

--- src/practicaLineUp/MainLineUp.java
+++ src/practicaLineUp/MainLineUp.java
@@ -1,7 +1,32 @@
 package practicaLineUp;
 
+import javax.sound.sampled.*;
+import java.applet.AudioClip;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 public class MainLineUp {
+
     public static void main(String[] args) {
-        System.out.println("");
+         final File archivowav;
+         Clip clip;
+         AudioInputStream audioInputStream;
+
+        archivowav = new File(
+                FileSystems.getDefault().getPath("")
+                        .toAbsolutePath() + "/audio/concierto1.wav");
+
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(archivowav);
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public static void play(){
+
     }
 }
